b36923193854765e1464fc1cb71cac11e06ae971
Godwin-munan <godwinjethro@gmail.com>
2025-03-06 10:36:46 +0100

    Design home section v1

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -11,14 +11,9 @@ import { Component, HostListener } from '@angular/core';
 })
 export class HeaderComponent {
 
-  mobileMenuOpen = false;
-  scrolled = false;
-
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    // When scrolling, add a shadow if scrollY > 0
-    this.scrolled = window.scrollY > 0;
-  }
+  mobileMenuOpen: boolean = false;
+  mdNavStyle: string = "px-3 py-2 text-sm font-medium text-gray-700 uppercase hover:text-gray-900 hover:border-b-2 hover:border-gray-900 transition duration-300 ease-in-out";
+  smMobileNavStyle: string = "block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900";
 
   toggleMobileMenu(): void {
     this.mobileMenuOpen = !this.mobileMenuOpen;
