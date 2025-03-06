5c479c430a9c38fb3c82257cabef916b9260e073
Godwin-munan <godwinjethro@gmail.com>
2025-03-06 10:42:16 +0100

    Competitors section setup

---
+++ src/app/competitors/competitors.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-competitors',
+  imports: [],
+  templateUrl: './competitors.component.html',
+  styleUrl: './competitors.component.scss'
+})
+export class CompetitorsComponent {
+
+}
