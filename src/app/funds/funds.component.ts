975662f3da440af5a20c88a9fd4c5fae98685fb5
Godwin-munan <godwinjethro@gmail.com>
2025-03-06 10:43:06 +0100

    Funds section setup

---
+++ src/app/funds/funds.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-funds',
+  imports: [],
+  templateUrl: './funds.component.html',
+  styleUrl: './funds.component.scss'
+})
+export class FundsComponent {
+
+}
