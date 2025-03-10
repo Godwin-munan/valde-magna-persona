3808ba7a0ba98fa9d96e138d2be2fdf6a3bbe5c9
Godwin-munan <godwinjethro@gmail.com>
2025-03-10 14:26:35 +0100

    Created custom tailwind class and loaders on config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 
-export const content = ["./**/*.{html,ts}"];
-export const theme = {
-  screens: {
-    sm: "340px",
-    md: "540px",
-    lg: "768px",
-    xl: "1180px"
-  },
-  extend: {},
-  fontFamily: {
-    Jost: ["Jost", "sans-serif"],
-    Lobster: ["Lobster", "sans-serif"]
-  },
-  container: {
-    center: true,
-    padding: {
-      DEFAULT: "12px",
-      md: "32px"
+module.exports = {
+  content: ["./src/**/*.{html,ts,scss,css}"],
+  theme: {
+    screens: {
+      sm: "370px",
+      md: "540px",
+      lg: "900px",
+      xl: "1920px"
+    },
+    extend: {
+      keyframes: {
+        rotate: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+        beep: {
+          '0%, 100%': { transform: 'scale(1)' },
+          '50%': { transform: 'scale(1.2)' },
+        },
+      },
+      animation: {
+        rotate: 'rotate 2s linear infinite',
+        beep: 'beep 1.5s ease-in-out infinite',
+      },
+    },
+    fontFamily: {
+      Jost: ["Jost", "sans-serif"],
+      Lobster: ["Lobster", "sans-serif"]
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "12px",
+        md: "32px"
+      }
     }
-  }
+  },
+  plugins: [],
+  important: true,
 };
