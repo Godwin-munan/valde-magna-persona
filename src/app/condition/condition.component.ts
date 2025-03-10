6184a48d671c955164981c233e7a1d30c4837d10
Godwin-munan <godwinjethro@gmail.com>
2025-03-10 14:18:59 +0100

    Design Condition section v1

--- src/app/condition/condition.component.ts
+++ src/app/condition/condition.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, TrackByFunction } from '@angular/core';
 
 @Component({
   selector: 'app-condition',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './condition.component.html',
   styleUrl: './condition.component.scss'
 })
 export class ConditionComponent {
+
+  cards: any[] = [
+    {
+      id: 1,
+      imageUrl: "/assets/images/coins.jpg",
+      title: "Financial Barriers",
+      description: "High costs prevent access to essential healthcare services, leading to untreated conditions and worsening health outcomes"
+    },
+    {
+      id: 2,
+      imageUrl: "/assets/images/women_pounding_grain.jpg",
+      title: "Limited Awareness",
+      description: "Lack of education on disease prevention results in increased susceptibility to life-threatening illnesses"
+    },
+    {
+      id: 3,
+      imageUrl: "/assets/images/child_and_older_sibling.jpg",
+      title: "Inequality",
+      description: "The disparities in healthcare access between these at-risk groups and the broader population are excessively large"
+    }
+  ]
+  
+  trackById(index: number, card: any): number {
+    return card.id;
+  }
 
 }
